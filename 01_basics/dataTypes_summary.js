05bf52e95f619bf3af914e6423d01e8223f079f9
shahid-dev78 <alushahid721@gmail.com>
2025-08-08 17:33:05 +0000

    summary done

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,7 @@
+// Primitive => Stack Memory(Copy)
+
+// 7 Types : string,number,boolean,null,undefined,symbol,BigInt
+
+// Reference Type (Non Primitive) => Heap Memory(Original)
+
+// Array, Objects,Functions
