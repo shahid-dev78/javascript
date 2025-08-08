e74879a83234653d99e76f40f4a0f695089313e4
shahid-dev78 <alushahid721@gmail.com>
2025-08-08 16:28:23 +0000

    Conversion of dataType

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,19 @@
+let score = false;
+
+let ValueInNum = Number(score);
+
+// console.log(typeof score);
+// console.log(ValueInNum);
+
+// console.log(typeof ValueInNum);
+
+// "33" => 33
+// "33djshd" => NaN
+// true => 1, False => 0
+// 1 => true ,0 => false
+// "Shahid" => true
+
+let num = 33;
+let strNum = String(num);
+console.log(strNum);
+console.log(typeof strNum);
