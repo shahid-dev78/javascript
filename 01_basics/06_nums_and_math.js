bc692cd45d009de13690c831848d608a71306d1a
shahid-dev78 <alushahid721@gmail.com>
2025-08-09 15:48:52 +0000

    Math and numbers done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,25 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(200);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+const num = 1000000;
+// console.log(num.toLocaleString("en-IN"));
+
+// ================ Math ================
+
+// console.log(Math);
+// console.log(Math.abs(-10));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+console.log(Math.random());
+// console.log(Math.random() * 10 + 1);
+const min = 10;
+const max = 20;
+const res = Math.floor(Math.random() * (max - min + 1) + min);
+console.log(res);
