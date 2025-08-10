92f0f9a33c83eb8458881fd744766701e0b0cb1f
shahid-dev78 <alushahid721@gmail.com>
2025-08-10 07:37:32 +0000

    date and array basics don

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,8 @@
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.toDateString());
+// console.log(newDate.toISOString());
+console.log(newDate.toLocaleDateString());
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getTime());
