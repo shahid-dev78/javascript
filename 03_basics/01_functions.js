fffd11800c74c853cfd6bb710fac28abdeec84cf
shahid-dev78 <alushahid721@gmail.com>
2025-08-10 13:38:54 +0000

    func done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -24,5 +24,29 @@ function loginUser(userName = "sam") {
   return `${userName} just logged in`;
 }
 
-result = loginUser();
-console.log(result);
+// result = loginUser();
+// console.log(result);
+
+function calcCartPrice(...num1) {
+  return num1;
+}
+// console.log(calcCartPrice(200, 4994, 989));
+
+const user = {
+  userName: "Ali",
+  price: 199,
+};
+
+function handleObj(anyObj) {
+  console.log(`Username is ${anyObj.userName} and price is ${anyObj.price}`);
+}
+
+// handleObj(user);
+
+const arr = [200, 438, 2432, 355];
+
+function returnSecVal(getArray) {
+  return getArray[1];
+}
+
+console.log(returnSecVal([100, 200, 200]));
