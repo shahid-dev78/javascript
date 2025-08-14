79fd047c457e41b49fb5f95d1a27fab7dfda9b3e
shahid-dev78 <alushahid721@gmail.com>
2025-08-14 08:23:40 +0500

    Delete 01_basics directory

--- 01_basics/dataTypes_summary.js
+++
@@ -1,7 +0,0 @@
-// Primitive => Stack Memory(Copy)
-
-// 7 Types : string,number,boolean,null,undefined,symbol,BigInt
-
-// Reference Type (Non Primitive) => Heap Memory(Original)
-
-// Array, Objects,Functions
